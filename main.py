267df332652ae62b9f3a64d0bc13c8619b6b63ae
Gabriel Corduna <31183067+gabic12@users.noreply.github.com>
2025-05-23 12:10:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import requests
 
-API_KEY = "YOUR API KEY"
+API_KEY = "8e39e59f020f4a1bbfa123543252005"
 CITY_NAME = "Bucharest"
 
-api_parameters = {
+weather_api_parameters = {
     "key": API_KEY,
     "q": CITY_NAME,
-    "aqi": "yes"
+    "aqi": "yes",
+    "day_fields": "daily_chance_of_rain",
+    "astro": "sunrise,sunset,moon_phase"
 }
 
 # API call
-response = requests.get(url="http://api.weatherapi.com/v1/current.json", params=api_parameters)
-response.raise_for_status()
-weather_data = response.json()
+weather_response = requests.get(url="http://api.weatherapi.com/v1/forecast.json", params=weather_api_parameters)
+weather_response.raise_for_status()
+weather_data = weather_response.json()
 
 # Weather variables
 date = weather_data["current"]["last_updated"]
 temp_c = weather_data["current"]["temp_c"]
 condition = weather_data["current"]["condition"]["text"]
 wind_kph = weather_data["current"]["wind_kph"]
+cloud = weather_data["current"]["cloud"]
+chance_of_rain = weather_data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
 uv = float(weather_data["current"]["uv"])
+sunrise = weather_data["forecast"]["forecastday"][0]["astro"]["sunrise"]
+sunset = weather_data["forecast"]["forecastday"][0]["astro"]["sunset"]
+moon_phase = weather_data["forecast"]["forecastday"][0]["astro"]["moon_phase"]
 
 # Air quality variables
 co = weather_data["current"]["air_quality"]["co"]
@@ -57,7 +64,8 @@ elif us_epa_index == 5:
 else:
     aqi = "Hazardous"
 
+print(f"Sunrise: {sunrise}\nSunset: {sunset}\nMoon phase: {moon_phase}\n")
 print(f"{CITY_NAME} at {date} was {condition}:\n- Temperature: {temp_c}°C\n- Wind speed: {wind_kph} km/h\n"
-      f"- UV index: {uv} ({uv_risk} risk of harm!)\n")
+      f"- Cloud coverage: {cloud}%\n- Chance of rain: {chance_of_rain}%\n- UV index: {uv} ({uv_risk} risk of harm!)\n")
 print(f"Air quality is {aqi}:\n- Carbon Monoxide: {co} μg/m3\n- Ozone: {o3} μg/m3\n- Nitrogen dioxide: {no2} μg/m3\n"
       f"- Sulphur dioxide: {so2} μg/m3\n- PM2.5: {pm2_5} μg/m3\n- PM10 {pm10} μg/m3")
